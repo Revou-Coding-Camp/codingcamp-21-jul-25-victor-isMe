18ac01ce407e71ce2774e042e66d7f5e359fb2d1
victor-isMe <muhammadfaizniwansyah@gmail.com>
2025-07-25 17:12:57 +0700

    membuat html,css, tetapi js masih bekum fix dan masih ada perubahan lagi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
+const taskInput = document.querySelector(".input-task"),
+    dateInput = document.querySelector(".input-date"),
+    filter = document.querySelectorAll(".task-filter span"),
+    clearAll = document.querySelector(".clear-button"),
+    taskList = document.querySelector(".task-list");
 
+//initialize variable
+let editId,
+    isEditTask = false,
+    todos = JSON.parse(localStorage.getItem("todo-list"));
+
+//event listeners for filter buttons
+filter.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        showTodo(btn.id);
+    });
+});
+
+//function to display tasks based on filter
+function showTodo(filter) {
+    let Litag = "";
+    if (todos) {
+        todos.forEach(todo, id) => {
+            //determine if tasks is completed or pending
+            let completed = todo.status == "completed" ? "checked" : "";
+            if (filter == todo.status || filter == "all"){
+                //create tasks list item
+                liTag += '<li class="task-value">
+                            <span>Task</span>
+                                <div class="subtools">
+                                    <span><i class="fa fa-pencil-square-o" style="color: green;"></i><i class="fa fa-trash" style="color:red;"></i></span>
+                                </div>
+                            </li>'
+            }
+        }
+    }
+}
